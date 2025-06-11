3bba14a965baa20d8460648a58b5500bcbd54468
yujiponzu <imaybeyujiueda@gmail.com>
2025-06-11 19:11:33 +0900

    feat: トップページに戻るボタンのコンポーネントを作成

---
+++ src/components/TopBackButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const TopBackButton: React.FC = () => (
+  <header className="w-full flex items-center py-4 mb-8">
+    <Link href="../" className="flex items-center">
+      <span className="text-2xl sm:text-3xl font-bold text-blue-600 pl-2 cursor-pointer">
+        {/* 仮アイコン．作成次第置き換え予定 */}
+        📘 Caffe-Run
+      </span>
+    </Link>
+  </header>
+);
+
+export default TopBackButton;
