e007bf0603c069b22b2a975af6c2640fb6f8e920
unknown <imaybeyujiueda@gmail.com>
2025-06-16 20:57:16 +0900

    fix: RecommendedPlanList要素とSummery要素の横幅を調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ const HomePage: React.FC = () => {
 
             {/* developブランチの新しいレイアウトを採用 */}
             <div className="w-full max-w-2xl mx-auto flex flex-row items-start justify-center gap-1 mt-8 px-0">
-              <div className="flex-1">
+              <div className="flex-2">
                 <RecommendedPlanList recommendations={recommendations} />
               </div>
               <div className="flex-1">
